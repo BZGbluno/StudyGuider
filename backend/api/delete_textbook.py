3fec22dd7e02ea5be8e729f1aaa7ad6058a1b1c0
Ben Schmidt <benschmidt2005@gmail.com>
2026-04-29 19:01:31 -0400

    Added s3 logic for delete_textbook

--- backend/api/delete_textbook.py
+++ backend/api/delete_textbook.py
@@ -6,6 +6,7 @@ import logging
 from uuid import UUID
 import uuid
 from api.auth import verify_jwt
+from api.s3 import delete_textbook_s3
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
@@ -42,8 +43,6 @@ async def delete_textbook(textbook_id: UUID, user_id = Depends(verify_jwt)):
 
         if result == "DELETE 0":
             raise HTTPException(status_code=404, detail="Textbook not found")
-        
-        return Response(status_code=204)
 
     except HTTPException:
         raise
@@ -55,3 +54,12 @@ async def delete_textbook(textbook_id: UUID, user_id = Depends(verify_jwt)):
     finally:
         if conn:
             await conn.close()
+
+    #now delete from s3
+    try:
+        delete_textbook_s3(supabase_uid, textbook_id)
+        logger.info(f"[{request_id}] Deleting from S3...")
+    except Exception as e:
+        logger.error(f"[{request_id}] S3 delete operation failed: {e}", exc_info=True)
+    
+    return Response(status_code=204)
--- backend/tests/test_delete_textbook_endpoint.py
+++ backend/tests/test_delete_textbook_endpoint.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from main import app
 from api.auth import verify_jwt
 import json
+from conftest import upload_fake_textbook, USER_ID, BUCKET
 
 def override_verify_jwt():
     return {"sub": "e6a6c7c5-4360-4722-8c0a-75ff8bff5b1f"}
@@ -10,10 +11,18 @@ def override_verify_jwt():
 app.dependency_overrides[verify_jwt] = override_verify_jwt
 
 @pytest.mark.asyncio
-async def test_delete_textbook_endpoint(client, db, textbook): 
+async def test_delete_textbook_endpoint(client, db, textbook, s3_bucket): 
     """
     Test deleting textbook associated with certain user
     """
+    upload_fake_textbook(s3_bucket, USER_ID, textbook)
+
+    before = s3_bucket.list_objects_v2(
+        Bucket=BUCKET,
+        Prefix=f"users/{USER_ID}/textbooks/{textbook}/"
+    )
+    assert "Contents" in before
+    
     # now delete via endpoint
     response = client.delete(f"/api/delete_textbook?textbook_id={str(textbook)}") 
 
@@ -26,3 +35,9 @@ async def test_delete_textbook_endpoint(client, db, textbook):
         "e6a6c7c5-4360-4722-8c0a-75ff8bff5b1f",  
     )
     assert deleted is None  
+
+    after = s3_bucket.list_objects_v2(
+        Bucket=BUCKET,
+        Prefix=f"users/{USER_ID}/textbooks/{textbook}/"
+    )
+    assert after.get("Contents") is None
